a2089a2306a3498983d800aeb2410622a8b1123a
freshwaterlemon <nguiyaksiong@gmail.com>
2025-03-19 20:20:40 +0800

    setup GitHub workflow

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/little-lemon-meta-capstone/',
   test: {
     environment: 'jsdom',
     globals: true,
